5d8ceede6e79dc8223fcb25f88a7b5519f66d681
hei karata <herishine00@gmail.com>
2024-10-01 10:40:36 +0300

    Add User and Role models; implement PasswordService
    Removed WeatherForecast class from IssueTracker.Api namespace.
    Added Role class in IssueTracker.Api.Models with Id and Name properties.
    Added User class in IssueTracker.Api.Models with Id, FirstName, LastName, Username, and Password properties.
    Implemented PasswordService in IssueTracker.Api.Services with methods for hashing and verifying passwords using PBKDF2 with HMACSHA512.

---
+++ src/Application/IssueTracker.Api/IssueTracker.Api/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Api.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ src/Application/IssueTracker.Api/IssueTracker.Api/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string LastName { get; set; } = string.Empty;
+
+        [MaxLength(40)]
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
